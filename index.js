d35757a8fc65c39d5f0f32a94e09d7418690e947
github-cloudlabsuser-098 <80267227+github-cloudlabsuser-098@users.noreply.github.com>
2024-11-20 16:42:55 +0000

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ var mkdirp = require('mkdirp')
 var fs = require('fs')
 var path = require('path')
 var os = require('os')
-var secret = "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" 
-  
+var secret = "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"
+
 var win32 = os.platform() === 'win32'
 
 var noop = function () {}
