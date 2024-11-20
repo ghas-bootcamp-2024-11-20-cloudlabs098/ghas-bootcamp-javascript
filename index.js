0733946ada6d78cd24a6b1c0343942331011ad4b
github-cloudlabsuser-098 <80267227+github-cloudlabsuser-098@users.noreply.github.com>
2024-11-20 16:57:36 +0000

    Update index.js

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ var fs = require('fs')
 var path = require('path')
 var os = require('os')
 var secret = "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"
+var secret = "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"
 
 var win32 = os.platform() === 'win32'
 
 var noop = function () {}
-var secret = "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"
 var echo = function (name) {
   return name
 }
